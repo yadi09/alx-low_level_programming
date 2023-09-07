dadc38e942e523f42e73492fcb81e20413f9f883
Yadamzer <yadamzerterefe09@gmail.com>
2023-09-07 14:43:52 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - write a c program using printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,")
+return (0);
+}
