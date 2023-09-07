ad5f25895ef84b815ed14e4ef48005d933091347
Yadamzer <yadamzerterefe09@gmail.com>
2023-09-07 16:23:01 -0700

    task 8 fix error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - prints exactly "and that piece of art is useful"
- *
- * Return: 0
+ * - Dora Korpar, 2015-10-19",
+ *   followed by a new line, to the standard error.
+ *   Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
