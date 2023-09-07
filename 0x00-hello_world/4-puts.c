14d4a5ef92f31a11e7cf0b5798e79984d4a807e9
Yadamzer <yadamzerterefe09@gmail.com>
2023-09-07 13:52:27 -0700

    task 4 fix the error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main-print a string using puts
+ *
+ * Return: 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
