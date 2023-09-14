445d71d25b69103f4685aacfaff66f00d0bdc2df
Yadamzer <yadamzerterefe09@gmail.com>
2023-09-13 18:19:20 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - add return int value
+ *
+ * @num1: parameter1
+ * @num2: parameter2
+ *
+ * Return: 0;
+ */
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
