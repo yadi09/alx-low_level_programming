ce2aca8d7cce197d8094c1c3889556f6644453c4
Yadamzer <yadamzerterefe09@gmail.com>
2023-09-18 05:57:22 -0700

    task 4 fix the error

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 char *str;
-str = "I do not fear computers. I fear the lack of them - Isaac Asimov"; 
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
 print_rev(str);
 return (0);
 }
