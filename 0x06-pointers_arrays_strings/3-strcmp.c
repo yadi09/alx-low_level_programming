4665a6902719aaacb289f017f441a1028830b429
Yadamzer <yadamzerterefe09@gmail.com>
2023-09-20 13:04:06 -0700

    task 3 trying to fix the error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,17 +7,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+while (*s1 != '\0' && *s2 != '\0')
 {
-if (s1[i] < s2[i])
+if (*s1 < *s2)
 return (-1);
-else if (s1[i] > s2[i])
+else if (*s1 > *s2)
 return (1);
+s1++;
+s2++;
 }
-if (s1[i] == '\0' && s2[i] != '\0')
+
+if (*s1 == '\0' && *s2 != '\0')
 return (-1);
-else if (s1[i] != '\0' && s2[i] == '\0')
+else if (*s1 != '\0' && *s2 == '\0')
 return (1);
 else
 return (0);
