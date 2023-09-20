cf39ecb109d5ea049b0df2a8642fabaa180af685
Yadamzer <yadamzerterefe09@gmail.com>
2023-09-20 08:45:37 -0700

    task 0 up to task 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat - function
+ * this program concatenates two strings.
+ * @dest: parameter char
+ * @src: parameter char
+ * @n: parameter n
+ * Return: 0;
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+  int dmax, dsrt = 0, i;
+while (dest[dsrt] != '\0')
+dsrt++;
+dmax = sizeof(dest);
+for (i = 0; i < dmax && src[i] != '\0' && i < n; i++)
+{
+dest[dsrt] = src[i];
+dsrt++;
+}
+dest[dsrt] = '\0';
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This readme file for directory 0x06-pointers_arrays_strings and it stores the a lot of task that concern about More pointers, arrays and strings
