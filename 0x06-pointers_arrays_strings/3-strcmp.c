edd8c44e5af96f863c88c40626f16bb5ce49de97
Yadamzer <yadamzerterefe09@gmail.com>
2023-09-20 13:05:35 -0700

    task 3 fix error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,17 @@ int _strcmp(char *s1, char *s2)
 while (*s1 != '\0' && *s2 != '\0')
 {
 if (*s1 < *s2)
-return (-1);
+return (-15);
 else if (*s1 > *s2)
-return (1);
+return (15);
 s1++;
 s2++;
 }
 
 if (*s1 == '\0' && *s2 != '\0')
-return (-1);
+return (-15);
 else if (*s1 != '\0' && *s2 == '\0')
-return (1);
+return (15);
 else
 return (0);
 }
