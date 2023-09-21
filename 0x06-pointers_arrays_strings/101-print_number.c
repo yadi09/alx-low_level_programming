9d3ec33f07d3feb1668bdcee8845b41c5d5f37df
Yadamzer <yadamzerterefe09@gmail.com>
2023-09-21 14:16:17 -0700

    task 9 fix error

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,18 +7,19 @@
  */
 void print_number(int n)
 {
+int n2;
 if (n < 0)
 {
 _putchar('-');
 n = n * -1;
 }
-
-if (n / 10)
+n2 = n;
+if (n2 / 10)
 {
-print_number(n / 10);
+print_number(n2 / 10);
 }
 
-_putchar(n % 10 + '0');
+_putchar(n2 % 10 + '0');
 
 return;
 }
