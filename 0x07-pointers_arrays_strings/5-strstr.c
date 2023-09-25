932354cf861abf1572f29a0f3c373c40538ff400
Yadamzer <yadamzerterefe09@gmail.com>
2023-09-25 12:29:06 -0700

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strstr - function
+ * @haystack: parameter
+ * @needle: parameter
+ * Return: 0;
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0;
+
+while (*(haystack) != '\0')
+{
+if (*(needle + i) == *(haystack))
+{
+i++;
+if (*(needle + i) == '\0')
+{
+i = i - 1;
+return (haystack - i);
+}
+haystack++;
+}
+else
+{
+needle = needle - i;
+haystack++;
+}
+}
+
+return ('\0');
+}
