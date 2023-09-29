6cfd4df41b4c06109b6cf1bc479d3c4b1c4e8deb
Yadamzer <yadamzerterefe09@gmail.com>
2023-09-29 13:05:08 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _strlen - function
+ * @s: parameter
+ * Return: 0;
+ */
+
+int _strlen(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (_strlen(++s) + 1);
+}
+
+/**
+ * is_same - function
+ * @s: parameter
+ * @n: parameter
+ * @num: parameter
+ * Return: 0;
+ */
+
+int is_same(char *s, int n, int num)
+{
+if (n == 0 || n < num)
+return (1);
+else if (*s == *(s + n - 1))
+return (is_same(++s, n - 2, num) * 1);
+else
+return (0);
+}
+
+
+
+/**
+ * is_palindrome - function
+ * @s: parameter
+ * Return: 0;
+ */
+
+int is_palindrome(char *s)
+{
+int num = _strlen(s);
+return (is_same(s, num, num / 2));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,6 @@ int _sqrt_recursion(int n);
 int _sqrt(int i, int j);
 int is_prime_number(int n);
 int PC(int num, int n);
+int is_palindrome(char *s);
 
 #endif
