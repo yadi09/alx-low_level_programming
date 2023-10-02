4316c66b368d8aa05d621d5e50ec8c55bc34dd7a
Yadamzer <yadamzerterefe09@gmail.com>
2023-10-02 14:33:51 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - argc and argv
+ * Return: 0;
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this directory (0x0A-argc_argv) holds all tasks with thire solution
