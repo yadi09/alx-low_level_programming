a1b3345dd02ba6f6a84f5c6195c8e6c96180e13a
Yadamzer <yadamzerterefe09@gmail.com>
2023-10-03 13:37:16 -0700

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - function that returns a pointer
+ * @str: char parameter
+ * Return: 0;
+ */
+
+char *_strdup(char *str)
+{
+int i, max = 0;
+char *ptr;
+if (str == NULL)
+return ('\0');
+
+while (*(str + max) != '\0')
+max++;
+
+ptr = (char *)malloc(sizeof(char) * max);
+if (ptr == NULL)
+return ('\0');
+else
+for (i = 0; i < max; i++)
+ptr[i] = str[i];
+
+return (ptr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,5 +3,6 @@
 
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
+char *_strdup(char *str);
 
 #endif
