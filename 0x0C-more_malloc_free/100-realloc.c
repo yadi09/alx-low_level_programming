7ce95351d48f95a7a23f671737aa836190a1a900
Yadamzer <yadamzerterefe09@gmail.com>
2023-10-06 14:14:56 -0700

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 unsigned int i;
 char *ptr2;
 
-if (new_size == 0)
+if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
 return ('\0');
