bf1a0073d687feb89888ee2adb2276367a07afa8
Yadamzer <yadamzerterefe09@gmail.com>
2023-10-09 12:00:22 -0700

    fix task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,21 +10,10 @@
 
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-return;
-
-if (d->name == NULL)
-printf("Name: (nil)\n");
-else
-printf("Name: %s\n", d->name);
-
-if (d->age == '\0')
-printf("Age: 0\n");
-else
-printf("Age: %f\n", d->age);
-
-if (d->owner == NULL)
-printf("Owner: (nil)\n");
-else
-printf("Owner: %s\n", d->owner);
+if (d != NULL)
+{
+printf("Name: %s\n", (d -> name) ? (d -> name) : "(nil)");
+printf("Age: %f\n", (d -> age) ? (d -> age) : 0);
+printf("Owner: %s\n", (d -> owner) ? (d -> owner) : "(nil)");
+}
 }
