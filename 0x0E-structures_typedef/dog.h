566d547b38c709c44998fee4c5f2d6054512a703
Yadamzer <yadamzerterefe09@gmail.com>
2023-10-09 02:09:01 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - diff data for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ * Return: 0;
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
