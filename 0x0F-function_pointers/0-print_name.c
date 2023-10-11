00b3b500bca54688633f63a14082eaaf81786b98
Yadamzer <yadamzerterefe09@gmail.com>
2023-10-11 09:52:07 -0700

    task 0 fix error

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdlib.h>
+#include<stdlib.h>
 /**
  * print_name - function that prints a name.
  * @name: for name string
@@ -11,5 +11,5 @@ void print_name(char *name, void (*f)(char *))
 {
 if (name != NULL)
 f(name);
-return;
+
 }
