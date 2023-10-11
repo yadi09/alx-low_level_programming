bdfebb87e407a3e2f2b70b4afe1f8c3cd115572f
Yadamzer <yadamzerterefe09@gmail.com>
2023-10-11 11:41:40 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
-if (size <= 0)
+if (size <= 0 || array == NULL || cmp == NULL)
 return (-1);
 
-if (array == NULL || cmp == NULL)
-return (0);
 
 for (i = 0; i < size; i++)
 if (cmp(*(array + i)))
