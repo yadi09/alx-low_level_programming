96a4469b3ff5847f9cd7cdef517a6184cddfb1db
Yadamzer <yadamzerterefe09@gmail.com>
2023-10-11 09:14:28 -0700

    0x0F-function_pointers with task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - function that prints a name.
+ * @name: for name string
+ * @f: pointer to function
+ * Return: void;
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNCPOINT
+#define FUNCPOINT
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
