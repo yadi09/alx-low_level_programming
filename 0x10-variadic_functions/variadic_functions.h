8fa3385b5e7b97d4d124a8f349e7183dd4759b74
Yadamzer <yadamzerterefe09@gmail.com>
2023-10-12 12:19:47 -0700

    0x10-variadic_functions with task 0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+
+#endif
