b7df567f1907708212d5c3b37da575c6ccc6ee9f
Yadamzer <yadamzerterefe09@gmail.com>
2023-10-19 18:14:57 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - ..
+ * @h: ..
+ * Return: number of nodes
+ **/
+
+size_t print_list(const list_t *h)
+{
+	unsigned int n = 0;
+
+	if (h == NULL)
+	return (-1);
+
+	while (h != NULL)
+	{
+	n++;
+	printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	}
+
+	return (n);
+}
