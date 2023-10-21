10f9dc772ee9da6ecd1362e6d0323309e38eda58
Yadamzer Terefe <yadamzerterefe09@gmail.com>
2023-10-21 18:25:51 +0100

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,14 +1,16 @@
 #include "lists.h"
 
 /**
- * 
+ * list_len - count element of linked list
+ * @h: linked list
+ * Return: number of element
 */
 
 size_t list_len(const list_t *h)
 {
-size_t count = 0;
+long unsigned int count = 0;
 
-while (h->next)
+while (h)
 {
 count++;
 h = h->next;
