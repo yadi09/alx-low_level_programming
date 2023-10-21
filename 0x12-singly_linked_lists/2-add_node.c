8eb546a827a48304db7bb2761a814712ab93d917
Yadamzer Terefe <yadamzerterefe09@gmail.com>
2023-10-21 19:50:37 +0100

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_node - add node to beginning
+ * @head: address of the 1st element
+ * @str: string
+ * Return: the address of the new element
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *newnode = malloc(sizeof(list_t));
+    char *dupstr = NULL;
+    int count = 0;
+
+    if (str == NULL)
+    return ('\0');
+
+    dupstr = strdup(str);
+
+    while (dupstr[count] != '\0')
+        count++;
+
+    newnode->str = malloc(sizeof(char) * count);
+
+    if (newnode->str == NULL)
+    return ('\0');
+
+    newnode->str = dupstr;
+    newnode->next = *head;
+    newnode->len = count;
+    *head = newnode;
+
+    return (*head);
+}
