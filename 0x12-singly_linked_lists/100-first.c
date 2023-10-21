cd3e77087072fda64774b6be7f6e5727e56fbbc4
Yadamzer Terefe <yadamzerterefe09@gmail.com>
2023-10-21 22:48:08 +0100

    task 5 fix error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,10 +6,10 @@
  * Return: void
 */
 
-void before_main() __attribute__ ((constructor));
+void before_main(void) __attribute__ ((constructor));
 
 
-void before_main()
+void before_main(void)
 {
 printf("You're beat! and yet, you must allow,\n"
 "I bore my house upon my back!\n");
