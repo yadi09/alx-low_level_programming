ee56068a1e60c48ed1f711c800cd092742821e83
Yadamzer <yadamzerterefe09@gmail.com>
2023-10-21 10:05:13 -0700

    correct betty error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,18 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t n = 0;
+size_t n = 0;
 
-    while (h != NULL)
-    {
-        if (!h->str)
-        printf("[0] (nil)\n");
-        else
-        printf("[%d] %s\n", h->len, h->str);
-        
-        n++;
-        h = h->next;
-    }
+while (h != NULL)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
 
-	return (n);
+n++;
+h = h->next;
+}
+
+return (n);
 }
