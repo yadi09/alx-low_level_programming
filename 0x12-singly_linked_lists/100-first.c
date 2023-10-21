8d0f9fc619112ed2b46cc13a114fdee5dafde41c
Yadamzer Terefe <yadamzerterefe09@gmail.com>
2023-10-21 22:44:03 +0100

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * 
+*/
+
+void before_main() __attribute__ ((constructor));
+
+
+void before_main()
+{
+    printf("You're beat! and yet, you must allow,\n"
+    "I bore my house upon my back!\n");
+}
