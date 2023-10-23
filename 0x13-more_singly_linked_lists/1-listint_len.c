4578ea1384086c1b8316464bfe3a5a90e7816838
Yadamzer Terefe <yadamzerterefe09@gmail.com>
2023-10-23 13:15:43 +0100

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: linked list
+ * Return: number of elements in a linked list
+*/
+
+size_t listint_len(const listint_t *h)
+{
+size_t num_of_elemrnt = 0;
+
+while (h != NULL)
+{
+h = h->next;
+num_of_elemrnt++; 
+}
+
+return (num_of_elemrnt);
+}
