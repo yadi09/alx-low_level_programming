4637c68f56716862a430b3ab7c3728262a125f28
Yadamzer Terefe <yadamzerterefe09@gmail.com>
2023-10-23 15:51:23 +0100

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: pointer to pointer to node
+ * Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+    listint_t *temp;
+
+    while (*head != NULL)
+    {
+        temp = (*head)->next;
+        free(*head);
+        *head = temp;
+    }
+    
+}
