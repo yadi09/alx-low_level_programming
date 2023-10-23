edcb1c60dde6699e12cdeeaf4741c949a579411e
Yadamzer Terefe <yadamzerterefe09@gmail.com>
2023-10-23 15:55:47 +0100

    Task 5 fix betty error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,13 @@
 
 void free_listint2(listint_t **head)
 {
-    listint_t *temp;
+listint_t *temp;
 
-    while (*head != NULL)
-    {
-        temp = (*head)->next;
-        free(*head);
-        *head = temp;
-    }
-    
+while (*head != NULL)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
+}
+
 }
