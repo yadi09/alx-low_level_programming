1024db5a532597050bcf4365d87acf104bf4071c
Yadamzer Terefe <yadamzerterefe09@gmail.com>
2023-10-24 22:56:53 +0100

    Task 10 fix error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index index of a listint_t linked list.
+ * delete_nodeint_at_index - deletes the node at index index of a listint_t.
  * @head: linked list
  * @index: index of node
  * Return: 1 if it succeeded, -1 if it failed
