c230e8b1ae5d9390c641ecf0638a414b90dd8153
Yadamzer Terefe <yadamzerterefe09@gmail.com>
2023-10-24 21:16:22 +0100

    Task 7 fix error

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 listint_t *temp = head;
 unsigned int i = 0;
 
-while (i < index)
+while (i <= index)
 {
 temp = temp->next;
 i++;
