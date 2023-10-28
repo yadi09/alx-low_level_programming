5901651dcc6391b0af744a8a3c692568deab6fa4
Yadamzer Terefe <yadamzerterefe09@gmail.com>
2023-10-28 15:01:48 +0100

    Task 0 fix betty error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
+unsigned int power_of(unsigned int num, unsigned int exponent);
+
+
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: pointing to a string of 0 and 1 chars
  * Return: converted number
 */
-
-unsigned int power_of(unsigned int num, unsigned int exponent);
 
 unsigned int binary_to_uint(const char *b)
 {
