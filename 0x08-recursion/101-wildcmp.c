dc70094a3064f4d53736e2618f39d1d593544bf6
yadamzer <yadamzerterefe09@gmail.com>
2023-11-15 15:55:54 -0800

    Task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * check - function
+ * @str1: ......
+ * @str2: .....
+ * @i: .....
+ * @j: ....
+ * Return: 0;
+ **/
+
+int check(char *str1, char *str2, int i, int j)
+{
+if (str1[i] == '\0' && str2[j] == '\0')
+	return (1);
+
+if (str1[i] == str2[j])
+	return (check(str1, str2, i + 1, j + 1));
+
+if (str1[i] == '\0' && str2[j] == '*')
+	return (check(str1, str2, i, j + 1));
+
+if (str2[j] == '*')
+	return (check(str1, str2, i + 1, j) || check(str1, str2, i, j + 1));
+
+return (0);
+}
+
+/**
+ * wildcmp - ....
+ * @s1: .........
+ * @s2: ......
+ * Return: 0;
+ **/
+
+int wildcmp(char *s1, char *s2)
+{
+return (check(s1, s2, 0, 0));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int prime(int num, int n);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
