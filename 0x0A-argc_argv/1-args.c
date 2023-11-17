75364d6d1113cff9742a459ef1fbb9d1cc974823
yadamzer <yadamzerterefe09@gmail.com>
2023-11-17 03:55:15 -0800

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: number of vector
+ * @argv: vector it self
+ * Return: 0;
+ **/
+
+int main(int argc, char __attribute__((unused))**argv)
+{
+printf("%d\n", --argc);
+return (0);
+}
