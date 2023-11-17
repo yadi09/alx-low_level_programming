1d37ce1d0b2523a0fcb89e200fef01f1cb1587cc
yadamzer <yadamzerterefe09@gmail.com>
2023-11-17 03:45:14 -0800

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: number of vector
+ * @argv: vector
+ * Return: 0;
+ **/
+
+int main(int __attribute__((unused)) argc, char **argv)
+{
+printf("%s\n", argv[0]);
+return (0);
+}
