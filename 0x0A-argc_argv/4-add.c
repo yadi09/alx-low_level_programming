9cfc04afd6948cac37216fc95bd157739a330621
yadamzer <yadamzerterefe09@gmail.com>
2023-11-17 04:49:00 -0800

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: number of vector
+ * @argv: vector it self
+ * Return: 0;
+ **/
+
+int main(int argc, char **argv)
+{
+int i, num, sum = 0;
+
+if (argc <= 1)
+{
+	printf("0\n");
+	return (1);
+}
+
+for (i = 1; i < argc; i++)
+{
+num = atoi(argv[i]);
+if (num != 0)
+	sum += atoi(argv[i]);
+else
+{
+	printf("Error\n");
+	return (-1);
+}
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
