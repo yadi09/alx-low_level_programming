122294ce58f3aed2f563bc14fec239551ca7e4fd
yadamzer <yadamzerterefe09@gmail.com>
2023-11-17 11:04:24 -0800

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,65 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-
 /**
- * _atoi - function that convert str to num
- * @str: string
- * Return: converted num
- **/
-
-int _atoi(char *str)
-{
-int i, num = 0, mult = 1;
-
-if (str == NULL)
-        return ('\0');
-
-for (i = 0; *str != '\0'; i++)
-{
-if (*str >= 48 && *str <= 57)
-{
-        num = (num * mult) + *str;
-        mult *= 10;
-        str++;
-}
-else
-        return (0);
-}
-
-return (num);
-}
-
-
-/**
- * main - main function
- * @argc: number of vector
- * @argv: vector it self
+ * main - adds positive numbers.
+ * @argc: command line argument
+ * @argv: command line argument
  * Return: 0;
- **/
+ */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int i, num, sum = 0;
+int i, j, sum = 0;
 
-if (argc == 1)
+if (argc <= 1)
 {
-        printf("%d\n", 0);
-        return (0);
+	printf("0\n");
+	return (0);
 }
 
-for (i = 1; i < argc; i++)
+for (j = 1; j < argc; j++)
 {
-num = _atoi(argv[i]);
+for (i = 0; argv[j][i]; i++)
+{
+if (argv[j][i] < '0' || argv[j][i] > '9')
+{
+	printf("Error\n");
+	return (1);
+}
+}
+}
 
-if (num != 0)
-        sum += atoi(argv[i]);
-else
-{
-        printf("Error\n");
-        return (1);
-}
-}
+for (i = 0; i < argc; i++)
+sum += atoi(argv[i]);
 
 printf("%d\n", sum);
 
