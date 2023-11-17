efe1d8c11c6e76e219efa8bf9f03841739f2e597
yadamzer <yadamzerterefe09@gmail.com>
2023-11-17 10:53:27 -0800

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,10 +12,10 @@ int main(int argc, char **argv)
 {
 int i, j, sum = 0;
 
-if (argc <= 1)
+if (argc == 1)
 {
-	printf("0\n");
-	return (1);
+	printf("%d\n", 0);
+	return (0);
 }
 
 for (i = 1; i < argc; i++)
