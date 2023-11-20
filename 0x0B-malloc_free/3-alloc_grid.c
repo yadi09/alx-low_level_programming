7d93cff9643d9a5eeef8a14c4fd1cb7207e71cb7
yadamzer <yadamzerterefe09@gmail.com>
2023-11-20 11:47:49 -0800

    Task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * alloc_grid - function that returns a pointer
+ * @width: width of array
+ * @height: height of array
+ * Return: 0
+ **/
+
+int **alloc_grid(int width, int height)
+{
+int **arr;
+int i, j;
+
+if (width <= 0 || height <= 0)
+	return ('\0');
+
+arr = malloc(sizeof(int *) * height);
+if (arr == NULL)
+	return ('\0');
+
+for (i = 0; i < height; i++)
+{
+*(arr + i) = malloc(sizeof(int) * width);
+if (*(arr + i) == NULL)
+{
+for (j = 0; j < i; j++)
+{
+	free(*(arr + i));
+	j++;
+}
+	free(arr);
+	return ('\0');
+}
+}
+
+
+for (i = 0; i < height; i++)
+{
+	for (j = 0; j < width; j++)
+	{
+	arr[i][j] = 0;
+	}
+}
+
+return (arr);
+}
