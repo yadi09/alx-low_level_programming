97d22646a6452fed378f720541a594fc132a98bb
yadamzer <yadamzerterefe09@gmail.com>
2023-11-20 10:36:00 -0800

    Task 2 fix code fail

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,11 @@ char *str_concat(char *s1, char *s2)
 int max1 = 0, max2 = 0, i = 0, j = 0;
 char *str_cat;
 
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
+
 while (s1[max1] != '\0')
 	max1++;
 
