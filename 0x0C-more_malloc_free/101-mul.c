ba8350576f3fbcf30a3314438843049a39769fc3
yadamzer <yadamzerterefe09@gmail.com>
2023-11-23 13:48:47 -0800

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,14 +10,27 @@
 
 int main(int argc, char *argv[])
 {
-int mul;
+unsigned long mul;
+	int i, j;
 
 if (argc != 3)
-	printf("Error\n");
-else
 {
-mul = atoi(argv[1]) * atoi(argv[2]);
-printf("%d\n", mul);
+	printf("Error\n");
+	exit(98);
 }
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] > 57 || argv[i][j] < 48)
+{
+	printf("Error\n");
+	exit(98);
+}
+}
+}
+mul = atol(argv[1]) * atol(argv[2]);
+printf("%lu\n", mul);
+
 return (0);
 }
