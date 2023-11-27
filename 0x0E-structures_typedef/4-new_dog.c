1f1ba896c1068d8303f81dd1629e55cd0a19b4e9
yadamzer <yadamzerterefe09@gmail.com>
2023-11-27 06:09:01 -0800

    Task 4 try to fix code fail

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,30 +35,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *new__dog;
     int max_name = 0, max_owner = 0;
-    char *dup_name, *dup_owner;
 
     while (name[max_name] != '\0')
     max_name++;
     while (owner[max_owner] != '\0')
     max_owner++;
 
-    dup_name = malloc(sizeof(char) * (max_name + 1));
-    if (dup_name == NULL)
-    return ('\0');
-    dup_owner = malloc(sizeof(char) * (max_owner + 1));
-    if (dup_owner == NULL)
-    {
-        free(dup_name);
-        return('\0');
-    }
-    
     new__dog = malloc(sizeof(dog_t));
     if (new__dog == NULL)
     return (NULL);
 
-    new__dog->name = _strcpy(dup_name, name);
+    new__dog->name = malloc(sizeof(char) * (max_name + 1));
+    if (new__dog->name == NULL)
+    {
+        free(new__dog);
+        return ('\0');
+    }
+    new__dog->owner = malloc(sizeof(char) * (max_owner + 1));
+    if (new__dog->owner == NULL)
+    {
+        free(new__dog->name);
+        free(new__dog);
+        return('\0');
+    }
+
+    new__dog->name = _strcpy(new__dog->name, name);
     new__dog->age = age;
-    new__dog->owner = _strcpy(dup_owner, owner);
+    new__dog->owner = _strcpy(new__dog->owner, owner);
 
     return (new__dog);
 }
