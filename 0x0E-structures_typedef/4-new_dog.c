b9c57f379f363bb6bff297fe25af3a23e8f709e3
yadamzer <yadamzerterefe09@gmail.com>
2023-11-27 06:15:23 -0800

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,35 +33,35 @@ return (dest);
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new__dog;
-    int max_name = 0, max_owner = 0;
+dog_t *new__dog;
+int max_name = 0, max_owner = 0;
 
-    while (name[max_name] != '\0')
-    max_name++;
-    while (owner[max_owner] != '\0')
-    max_owner++;
+while (name[max_name] != '\0')
+max_name++;
+while (owner[max_owner] != '\0')
+max_owner++;
 
-    new__dog = malloc(sizeof(dog_t));
-    if (new__dog == NULL)
-    return (NULL);
+new__dog = malloc(sizeof(dog_t));
+if (new__dog == NULL)
+return (NULL);
 
-    new__dog->name = malloc(sizeof(char) * (max_name + 1));
-    if (new__dog->name == NULL)
-    {
-        free(new__dog);
-        return ('\0');
-    }
-    new__dog->owner = malloc(sizeof(char) * (max_owner + 1));
-    if (new__dog->owner == NULL)
-    {
-        free(new__dog->name);
-        free(new__dog);
-        return('\0');
-    }
+new__dog->name = malloc(sizeof(char) * (max_name + 1));
+if (new__dog->name == NULL)
+{
+free(new__dog);
+return ('\0');
+}
+new__dog->owner = malloc(sizeof(char) * (max_owner + 1));
+if (new__dog->owner == NULL)
+{
+free(new__dog->name);
+free(new__dog);
+return ('\0');
+}
 
-    new__dog->name = _strcpy(new__dog->name, name);
-    new__dog->age = age;
-    new__dog->owner = _strcpy(new__dog->owner, owner);
+new__dog->name = _strcpy(new__dog->name, name);
+new__dog->age = age;
+new__dog->owner = _strcpy(new__dog->owner, owner);
 
-    return (new__dog);
+return (new__dog);
 }
