577348ee2ddc9085d40b303544afab83e3f94ece
yadamzer <yadamzerterefe09@gmail.com>
2023-11-27 05:29:17 -0800

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,18 +12,15 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-char *dup_name = strdup(name);
-char *dup_owner = strdup(owner);
-
-	dog_t *new_dog = malloc(sizeof(dog_t));
+dog_t *new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 	{
 	return (NULL);
 	}
 
-	new_dog->name = dup_name;
+	new_dog->name = name;
 	new_dog->age = age;
-	new_dog->owner = dup_owner;
+	new_dog->owner = owner;
 
 return (new_dog);
 }
