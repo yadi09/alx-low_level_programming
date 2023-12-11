e98f8c749aaced0edf481979dac2b99e41f30859
yadamzer <yadamzerterefe09@gmail.com>
2023-12-11 03:50:50 -0800

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function
+ * @head: .....
+ * Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+int i;
+
+if (*head == NULL && head == NULL)
+return;
+
+for (i = 0; *head != NULL; i++)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
+}
+}
