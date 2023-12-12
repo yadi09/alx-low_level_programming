d33584a519ed47ba950a4f6f2a5db8db7b86f119
yadamzer <yadamzerterefe09@gmail.com>
2023-12-12 11:54:32 -0800

    Task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - function
+ * @head: .....
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+long int num;
+size_t i = 0;
+
+while (head)
+{
+num = head - head->next;
+i++;
+printf("[%p] %d\n", (void *)head, head->n);
+if (num > 0)
+	head = head->next;
+else
+{
+	printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+	break;
+}
+}
+
+return (num);
+}
