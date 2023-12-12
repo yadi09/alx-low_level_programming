024cff96edf4fd57a6af9e932c044dbef451c272
yadamzer <yadamzerterefe09@gmail.com>
2023-12-12 12:10:36 -0800

    Task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint_safe - function
+ * @h: ......
+ * Return: ........
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+size_t length = 0;
+int num;
+listint_t *temp;
+
+if (!h || !*h)
+	return (0);
+
+while (*h)
+{
+num = *h - (*h)->next;
+
+if (num > 0)
+{
+temp = (*h)->next;
+*h = temp;
+length++;
+}
+else
+{
+*h = NULL;
+length++;
+break;
+}
+}
+
+*h = NULL;
+
+return (length);
+}
