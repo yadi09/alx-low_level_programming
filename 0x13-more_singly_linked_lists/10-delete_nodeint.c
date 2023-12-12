61eb56a14cd73b175287bc48c0370bb9c534657a
yadamzer <yadamzerterefe09@gmail.com>
2023-12-12 10:14:27 -0800

    Task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - function
+ * @head: ....
+ * @index: .....
+ * Return: int
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+unsigned int i;
+listint_t *temp;
+listint_t *single_temp;
+
+if (*head == NULL || head == NULL)
+return (-1);
+temp = *head;
+
+if (index == 0)
+{
+single_temp = temp->next;
+free(temp);
+*head = single_temp;
+}
+
+for (i = 0; i <= index && temp != NULL; i++)
+{
+if (i == (index - 1))
+{
+single_temp = temp;
+temp = temp->next;
+single_temp->next = temp->next;
+free(temp);
+return (1);
+}
+temp = temp->next;
+}
+
+return (-1);
+}
