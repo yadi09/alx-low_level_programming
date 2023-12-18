ede1585e25993ac47dd3366cd63f52f695b611c2
yadamzer <yadamzerterefe09@gmail.com>
2023-12-18 13:48:18 -0800

    Task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - ............
+ * @filename: .....
+ * @text_content: ..........
+ * Return: ......
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, a, b = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[b])
+			b++;
+		a = write(fd, text_content, b);
+		if (a != b)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
