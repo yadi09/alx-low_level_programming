23ef48325ccaae2d0b0b8a7f7efce64fa8d39b2e
yadi09 <yadamzerterefe09@gmail.com>
2024-01-25 09:20:37 +0100

    Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,49 +2,45 @@
 
 /**
  * delete_dnodeint_at_index - function
- * @head: ......
- * @index: .........
+ * @head: ....
+ * @index: .....
  * Return: ...
  **/
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-  unsigned int i = 0;
   dlistint_t *temp = *head;
-  dlistint_t *temp_temp;
-  if (!head || !(*head))
+  unsigned int i = 0;
+
+  if (*head == NULL)
     return (-1);
 
-  if (temp->next == NULL && i == (index - 1))
+  if (index == 0)
     {
+      *head = (*head)->next;
+      if (*head != NULL)
+	(*head)->prev = NULL;
       free(temp);
       return (1);
     }
 
-  temp = temp->next;
-  while (temp)
+  while (temp != NULL)
     {
-      if (i == (index - 1))
+      if (i == index)
 	{
-	  temp_temp = temp->next;
-	  if (temp->next->next != NULL)
+	  temp->prev->next = temp->next;
+	  if (temp->next != NULL)
 	    {
-	      temp->next = temp->next->next;
-	      temp->next->next->prev = temp;
-	      
+	      temp->next->prev = temp->prev;
 	    }
-	  else
-	    {
-	      temp->next = NULL;
-	    }
-
-	  if (temp_temp != NULL)
-	    free(temp_temp);
-
+	  free(temp);
 	  return (1);
 	}
-      i++;
-      temp = temp->next;
+      else
+	{
+	  temp = temp->next;
+	  i++;
+	}
     }
 
   return (-1);
