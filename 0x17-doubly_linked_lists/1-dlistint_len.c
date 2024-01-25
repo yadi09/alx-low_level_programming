f591e5cddb6778b87536fd0a4efbe05126267869
yadamzer <yadamzerterefe09@gmail.com>
2024-01-24 22:57:05 -0800

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,13 +7,13 @@
  **/
 
 size_t dlistint_len(const dlistint_t *h)
-  {
-    size_t count = 0;
-    while (h != NULL)
-      {
-	count++;
-	h = h->next;
-      }
+{
+size_t count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
 
-    return (count);
-  }
+return (count);
+}
