58f112a90b8e190de01ecf66cd58a799ffdf0e57
yadamzer <yadamzerterefe09@gmail.com>
2024-01-24 23:08:30 -0800

    Task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -7,17 +7,17 @@
  **/
 
 int sum_dlistint(dlistint_t *head)
-  {
-    int sum = 0;
+{
+int sum = 0;
 
-    if (head == NULL)
-      return 0;
+if (head == NULL)
+return (0);
 
-    while (head)
-      {
-	sum += head->n;
-	head = head->next;
-      }
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
 
-    return (sum);
-  }
+return (sum);
+}
