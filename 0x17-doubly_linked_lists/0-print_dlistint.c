f4296db58cf56aac67bab028ac8bb833f65f614f
yadamzer <yadamzerterefe09@gmail.com>
2024-01-24 22:55:07 -0800

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,17 +7,17 @@
  **/
 
 size_t print_dlistint(const dlistint_t *h)
-  {
-    int count = 0;
-    if (!h)
-      return (0);
+{
+int count = 0;
+if (!h)
+return (0);
 
-    while (h)
-      {
-	printf("%d\n", h->n);
-	h = h->next;
-	count++;
-      }
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
 
-    return (count);
-  }
+return (count);
+}
