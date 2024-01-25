0e052f4aab433b4b632338a699d2f03a3b7ddb86
yadamzer <yadamzerterefe09@gmail.com>
2024-01-24 23:04:49 -0800

    Task 5

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,12 +8,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-  dlistint_t *temp;
+dlistint_t *temp;
 
-  while (head)
-    {
-      temp = head->next;
-      free(head);
-      head = temp;
-    }
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,20 +8,20 @@
  **/
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-  {
-    unsigned int i;
+{
+unsigned int i;
 
-    if (head == NULL)
-      return (NULL);
+if (head == NULL)
+return (NULL);
 
-    i = 0;
-    while (head)
-      {
-	if (i == index)
-	  return (head);
-	head = head->next;
-	i++;
-      }
+i = 0;
+while (head)
+{
+if (i == index)
+return (head);
+head = head->next;
+i++;
+}
 
-    return (NULL);
-  }
+return (NULL);
+}
