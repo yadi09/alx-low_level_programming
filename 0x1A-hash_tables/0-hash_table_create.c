7f063c21452abcbf35c9f19eba34c6f71b5eb3be
yadi09 <yadamzerterefe09@gmail.com>
2024-02-17 11:55:30 +0100

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,27 +9,21 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+hash_table_t *hash_table;
 unsigned long int i;
 
-hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+hash_table = malloc(sizeof(hash_table_t));
 if (hash_table == NULL)
 return (NULL);
 
 hash_table->size = size;
-hash_table->array = calloc(size, sizeof(hash_node_t *));
+hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 if (hash_table->array == NULL)
-{
-free(hash_table);
 return (NULL);
-}
 
-i = 0;
-while (i < size)
-{
+for (i = 0; i < size; i++)
 hash_table->array[i] = NULL;
-i++;
-}
 
 return (hash_table);
 }
