8d3d5c5c7d6f09b44e52f4d7cf7180e3fc0980ee
yadi09 <yadamzerterefe09@gmail.com>
2024-02-17 13:05:27 +0100

    Task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,15 @@
 #include "hash_tables.h"
 
 /**
- *
+ * key_index - function
+ * @key: key
+ * @size: size of array
+ * Return: unsigned long int
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-  unsigned long int hash_value = hash_djb2(key);
+unsigned long int hash_value = hash_djb2(key);
 
-  return (hash_value % size);
+return (hash_value % size);
 }
