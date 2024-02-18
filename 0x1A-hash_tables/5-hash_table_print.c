3e4f8835ff82be8eb518ca52ce398aa4dcc011ff
yadi09 <yadamzerterefe09@gmail.com>
2024-02-18 21:54:18 +0100

    Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,32 +8,32 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-  hash_node_t **node;
-  unsigned long int i, to_check = 0;
+hash_node_t **node;
+unsigned long int i, to_check = 0;
 
-  if (ht == NULL)
-    {
-      printf("{}\n");
-      return;
-    }
+if (ht == NULL)
+{
+printf("{}\n");
+return;
+}
 
-  node = ht->array;
-  printf("{");
+node = ht->array;
+printf("{");
 
-  for (i = 0; i < ht->size; i++)
-    {
-      while (node[i])
-	{
-	  printf("'%s': '%s'", node[i]->key, node[i]->value);
-	  to_check = 1;
-	  if (node[i]->next != NULL)
-	    {
-	      printf(", ");
-	    }
-	  node[i] = node[i]->next;
-	}
-      if (node[i + 1] && to_check == 1)
-	printf(", ");
-    }
-  printf("}\n");
+for (i = 0; i < ht->size; i++)
+{
+while (node[i])
+{
+printf("'%s': '%s'", node[i]->key, node[i]->value);
+to_check = 1;
+if (node[i]->next != NULL)
+{
+printf(", ");
+}
+node[i] = node[i]->next;
+}
+if (node[i + 1] && to_check == 1)
+printf(", ");
+}
+printf("}\n");
 }
