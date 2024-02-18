75eefdbef3595a46e9f84a55baf7222667571dbd
yadi09 <yadamzerterefe09@gmail.com>
2024-02-18 21:38:42 +0100

    Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function
+ * @ht: hash_table_t type
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+  hash_node_t **node;
+  unsigned long int i, to_check = 0;
+
+  if (ht == NULL)
+    {
+      printf("{}\n");
+      return;
+    }
+
+  node = ht->array;
+  printf("{");
+
+  for (i = 0; i < ht->size; i++)
+    {
+      if (node[i])
+	{
+	  printf("'%s': '%s'", node[i]->key, node[i]->value);
+	  to_check = 1;
+	}
+      if (node[i + 1] && to_check == 1)
+	printf(", ");
+    }
+  printf("}\n");
+}
