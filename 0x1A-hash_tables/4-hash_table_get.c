afbffe3a27ff335d9fe35ceb8c7174bc99a66ede
yadi09 <yadamzerterefe09@gmail.com>
2024-02-18 21:05:57 +0100

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,22 +9,22 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-  unsigned long int index;
-  hash_node_t *node;
+unsigned long int index;
+hash_node_t *node;
 
-  if (ht == NULL || key == NULL || strlen(key) == 0)
-    return (NULL);
+if (ht == NULL || key == NULL || strlen(key) == 0)
+return (NULL);
 
-  index = key_index((const unsigned char *)key, ht->size);
-  node = ht->array[index];
+index = key_index((const unsigned char *)key, ht->size);
+node = ht->array[index];
 
-  while (node != NULL)
-    {
-      if (strcmp(node->key, key) == 0)
-	return (node->value);
+while (node != NULL)
+{
+if (strcmp(node->key, key) == 0)
+return (node->value);
 
-      node = node->next;
-    }
+node = node->next;
+}
 
-  return (NULL);
+return (NULL);
 }
