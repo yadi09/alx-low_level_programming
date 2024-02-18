eea600a44fa117e77471278f19866570e78597db
yadi09 <yadamzerterefe09@gmail.com>
2024-02-18 20:28:35 +0100

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,7 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
   unsigned long int index;
   hash_node_t *new_node, *node;
 
-  if (key == NULL || strlen(key) == 0)
+  if (key == NULL || strlen(key) == 0 || ht == NULL)
     return (0);
 
   index = key_index((const unsigned char *)key, ht->size);
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -13,10 +13,10 @@ int main(void)
     hash_table_t *ht;
 
     ht = hash_table_create(1024);
-    hash_table_set(ht, "betty", "cool");
-    hash_table_set(ht, "Name", "Yadamzer");
-    hash_table_set(ht, "hetairas", "111");
-    hash_table_set(ht, "mentioner", "222");
+    hash_table_set(ht, "Holberton", "is cool");
+    hash_table_set(ht, "Holberton", "is soooo cooool");
+    hash_table_set(ht, "Holberton", "is awesome");
+
 
     return (EXIT_SUCCESS);
 }
