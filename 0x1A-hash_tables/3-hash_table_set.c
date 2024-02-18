da6fd63db274442c107ebcd2940e45396cc1f7dd
yadi09 <yadamzerterefe09@gmail.com>
2024-02-18 10:37:44 +0100

    Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,69 @@
+#include "hash_tables.h"
+
+/**
+ * create_new_node - function
+ * @key: key
+ * @value: value
+ * Return: new node
+ */
+
+hash_node_t *create_new_node(const char *key, const char *value)
+{
+  hash_node_t *node = malloc(sizeof(hash_node_t));
+  if (node == NULL)
+    return (NULL);
+
+  node->key = strdup(key);
+  node->value = strdup(value);
+  node->next = NULL;
+
+  return (node);
+}
+
+
+
+/**
+ * hash_table_set - function
+ * @ht: hash_table_set type
+ * @key: key
+ * @value: value
+ * Return: 0 or 1
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+  unsigned long int index;
+  hash_node_t *new_node, *node;
+
+  if (key == NULL || *key == '\n')
+    return (0);
+
+  index = key_index((const unsigned char *)key, ht->size);
+  node = ht->array[index];
+
+  if (node == NULL)
+    {
+    ht->array[index] = create_new_node(key, value);
+    return (1);
+    }
+
+  while (node != NULL)
+    {
+      if (strcmp(node->key, key) == 0)
+	{
+	  free(node->value);
+	  node->value = strdup(value);
+	  return (1);
+	}
+      node = node->next;
+    }
+
+  new_node = create_new_node(key, value);
+  if (new_node == NULL)
+    return (0);
+
+  new_node->next = ht->array[index];
+  ht->array[index] = new_node;
+
+  return (1);
+}
---
+++ 0x1A-hash_tables/3-main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "betty", "cool");
+    hash_table_set(ht, "Name", "Yadamzer");
+    hash_table_set(ht, "hetairas", "111");
+    hash_table_set(ht, "mentioner", "222");
+    
+    
+    return (EXIT_SUCCESS);
+}
